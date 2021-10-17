5f6669b694bca73e643b07098f052536dc20e5e2
GoLang <87151713+GoLang1337@users.noreply.github.com>
2021-10-17 16:45:16 +0200

    A shitty space

--- Dumper/Dumper/memory.cpp
+++ Dumper/Dumper/memory.cpp
@@ -5,7 +5,7 @@ HANDLE hProcess;
 uint64 Base;
 
 bool Read(void *address, void *buffer, uint64 size) {
-	mem->read_virtual_memory((void*)address, (void*)buffer, (uint64)size);
+    mem->read_virtual_memory((void*)address, (void*)buffer, (uint64)size);
     return true;
 }
 
